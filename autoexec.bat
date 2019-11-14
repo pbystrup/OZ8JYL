1e4c70616fe061634c8472a853509785f8a18ce8
Peter Bystrup <pbystrup@users.noreply.github.com>
2019-11-14 08:45:56 +0100

    Update autoexec.bat

--- autoexec.bat
+++ autoexec.bat
@@ -10,16 +10,26 @@ REM This is the label for the identifier DOS
 LH C:\DOS\MSCDEX.EXE /D:CD1
 LH C:\DOS\SMARTDRV.EXE 2048
 
+REM This is the label for the identifier FOX
+:FOX
+LH C:\FOX\Ræv.exe
+
+REM This is the label for the identifier PROM
+:PROM
+LH C:\PromBr\PromBr.exe
+
+REM This is the label for the identifier NC 
+:NC
+LH C:\NC551\NC.EXE
+
 REM The GOTO EXIT is required so it won't continue into 
 REM the WIN configuration
 GOTO EXIT
 
 REM This is the label for the identifier WIN 
 :WIN
-
 REM The ECHO command writes a message on the DOS screen 
 ECHO Starting Windows...
-
 C:\WINDOWS\WIN
 
 :EXIT
